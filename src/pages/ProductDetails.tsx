6b1d86588ed95a6a1c4fc1eec84a8b30ffd0739e
namiqmamedov <namigmm@code.edu.az>
2023-07-11 21:27:22 +0400

    Update product detail page using axios

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,7 +1,30 @@
+import { Typography } from "@mui/material"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { Product } from "../models/product";
 
 const ProductDetails = () => {
+    const {id} = useParams<{id: string}>();
+    const [product, setProduct] = useState<Product | null>(null)
+    const [loading,setLoading] = useState(true);
+
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/api/product/${id}`)
+        .then(response => setProduct(response.data))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false))
+    }, [id])
+
+    if(loading) return <h3>Loading ...</h3>
+    
+    if(!product) return <h3>Product not found</h3>
+
   return (
-    <div>ProductDetails</div>
+    <Typography variant='h2'>
+        {product.name}
+    </Typography>
   )
 }
 
