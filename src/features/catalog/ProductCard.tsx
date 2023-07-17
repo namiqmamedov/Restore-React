6278ab2f50501441d8e9b48ada63fcf176c59659
namiqmamedov <namigmm@code.edu.az>
2023-07-17 21:44:08 +0400

    Fixed loading indicator in add to cart button

--- src/features/catalog/ProductCard.tsx
+++ src/features/catalog/ProductCard.tsx
@@ -50,9 +50,47 @@ export default function ProductCard({product} : Props){
         </Typography>
       </CardContent>
       <CardActions>
-        <LoadingButton  loading={status.includes('pendingAddItem' + product.id)} onClick={() => dispatch(addBasketItemAsync({ productID: product.id}))} size="small" >Add to cart</LoadingButton>
+        <LoadingButton  loading={status === 'pendingAddItem' + product.id}
+         onClick={() => dispatch(addBasketItemAsync({ productID: product.id}))}
+         size="small">
+          Add to cart
+        </LoadingButton>
         <Button component={Link} to={`/product/${product.id}`} size="small">View</Button>
       </CardActions>
     </Card>
     )
 }
+
+{/* <Card>
+<CardHeader
+    avatar={
+        <Avatar sx={{ bgcolor: 'secondary.main' }}>
+            {product.name.charAt(0).toUpperCase()}
+        </Avatar>
+    }
+    title={product.name}
+    titleTypographyProps={{
+        sx: { fontWeight: 'bold', color: 'primary.main' }
+    }}
+/>
+<CardMedia
+    sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'primary.light' }}
+    image={product.pictureURL}
+    title={product.name}
+/>
+<CardContent>
+    <Typography gutterBottom color='secondary' variant="h5">
+        {currencyFormat(product.price)}
+    </Typography>
+    <Typography variant="body2" color="text.secondary">
+        {product.brand} / {product.type}
+    </Typography>
+</CardContent>
+<CardActions>
+    <LoadingButton 
+        loading={status === 'pendingAddItem' + product.id} 
+        onClick={() => dispatch(addBasketItemAsync({productID: product.id}))} 
+        size="small">Add to Cart</LoadingButton>
+    <Button component={Link} to={`/catalog/${product.id}`}  size="small">View</Button>
+</CardActions>
+</Card> */}
